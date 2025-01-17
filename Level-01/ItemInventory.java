363777b61f6089176650ac8340786bd896168d9a
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-17 12:33:21 +0530

    Added

---
+++ Level-01/ItemInventory.java
@@ -0,0 +1,29 @@
+public class ItemInventory {
+    public static void main(String[] args) {
+        Item item = new Item(101, "Example Item", 15.99);
+        item.displayItemDetails();
+        System.out.println("Total Cost for 2 items: $" + item.calculateTotalCost(2));
+    }
+}
+
+class Item {
+    private int itemCode;
+    private String itemName;
+    private double price;
+
+    public Item(int itemCode, String itemName, double price) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public void displayItemDetails() {
+        System.out.println("Item Code: " + itemCode);
+        System.out.println("Item Name: " + itemName);
+        System.out.println("Price: $" + price);
+    }
+
+    public double calculateTotalCost(int quantity) {
+        return price * quantity;
+    }
+}
