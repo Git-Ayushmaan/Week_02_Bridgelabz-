32930b90fd56e60a816007f016d4a3b144eeae47
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-17 12:28:50 +0530

    Added

---
+++ Level-02/PalindromeString.java
@@ -0,0 +1,27 @@
+public class PalindromeString {
+    public class PalindromeChecker {
+        private String text;
+
+        public PalindromeChecker(String text) {
+            this.text = text;
+        }
+
+        public boolean isPalindrome() {
+            String reversed = new StringBuilder(text).reverse().toString();
+            return text.equalsIgnoreCase(reversed);
+        }
+
+        public void displayResult() {
+            if (isPalindrome()) {
+                System.out.println("\"" + text + "\" is a palindrome.");
+            } else {
+                System.out.println("\"" + text + "\" is not a palindrome.");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        PalindromeString palindromeString = new PalindromeString();
+        PalindromeString.PalindromeChecker palindromeChecker = palindromeString.new PalindromeChecker("racecar");
+        palindromeChecker.displayResult();
+    }
+}
