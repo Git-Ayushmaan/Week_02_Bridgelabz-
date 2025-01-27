e2b8b857f0837749fcf831eb6cf64d324875f86b
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-27 10:03:06 +0530

    Assisted problems added successfully

---
+++ assistedproblems/animalhierarchy/Animal.java
@@ -0,0 +1,18 @@
+package com.inheritance.assistedproblems.animalhierarchy;
+
+// Superclass Animal
+class Animal {
+    String name;
+    int age;
+
+    // Constructor
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // General makeSound method to be overridden
+    public void makeSound() {
+        System.out.println("Animal makes different sounds");
+    }
+}
