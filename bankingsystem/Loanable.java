68211c0c42a5bddd375425df0c973a668389f65e
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-27 10:12:09 +0530

    banking system added successfully

---
+++ bankingsystem/Loanable.java
@@ -0,0 +1,7 @@
+package com.encapsulationandpolymorphism.bankingsystem;
+
+public interface Loanable {
+    public void applyForLoan(double amount);
+    public boolean calculateLoanEligibility();
+
+}
